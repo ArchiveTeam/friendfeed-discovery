0897d927c6f0e3f07a991795d4419f7a871c47f5
Arkiver2 <Arkiver@hotmail.com>
2015-04-03 14:50:31 +0200

    discover.py: add support for 'account'

--- discover.py
+++ discover.py
@@ -19,9 +19,10 @@ def main():
     # Normal programs use 'argparse' but this keeps things simple
     start_num = int(sys.argv[1])
     end_num = int(sys.argv[2])
-    item_value = sys.argv[3]
-    item_type = sys.argv[4]
-    output_filename = sys.argv[5]  # this should be something like myfile.txt.gz
+    end_num2 = int(sys.argv[3])
+    item_value = sys.argv[4]
+    item_type = sys.argv[5]
+    output_filename = sys.argv[6]  # this should be something like myfile.txt.gz
 
     assert start_num <= end_num
 
@@ -30,7 +31,7 @@ def main():
 
     gzip_file = gzip.GzipFile(output_filename, 'wb')
 
-    for shortcode in check_range(start_num, end_num, item_value, item_type):
+    for shortcode in check_range(start_num, end_num, end_num2, item_value, item_type):
         # Write the valid result one per line to the file
         line = '{0}\n'.format(shortcode)
         gzip_file.write(line.encode('ascii'))
@@ -40,13 +41,10 @@ def main():
     print('Done')
 
 
-def check_range(start_num, end_num, item_value, item_type):
+def check_range(start_num, end_num, end_num2, item_value, item_type):
     for num in range(start_num, end_num + 1):
         shortcode = num
-        if item_type == 'page':
-            url = 'http://friendfeed.com/groups/search?q={0}&start={1}'.format(item_value, str((int(shortcode)-1)*48))
-        elif item_type == 'group':
-            url = 'http://friendfeed.com/{0}/subscribers?start={1}'.format(item_value, str((int(shortcode)-1)*80))
+        url = 'http://friendfeed.com/{0}/subscribers?start={1}'.format(item_value, str((int(shortcode)-1)*80))
         counter = 0
 
         while True:
@@ -67,18 +65,49 @@ def check_range(start_num, end_num, item_value, item_type):
                     if item_type == 'page':
                         yield 'page:{0}:{1}'.format(item_value, shortcode)
                         print('page:{0}:{1}'.format(item_value, shortcode))
-                    elif item_type == 'group':
-                        yield 'group:{0}:{1}'.format(item_value, shortcode)
-                        print('group:{0}:{1}'.format(item_value, shortcode))
+                    elif item_type == 'account':
+                        yield 'account:{0}:{1}'.format(item_value, shortcode)
+                        print('account:{0}:{1}'.format(item_value, shortcode))
                     sys.stdout.flush()
 
                     for file in extract_files(text):
-                        if item_type == 'page':
-			    yield 'group:{0}'.format(file)
-                            print('group:{0}'.format(file))
-                        elif item_type == 'group':
-			    yield 'profile:{0}'.format(file)
-                            print('profile:{0}'.format(file))
+			yield 'account:{0}'.format(file)
+                        print('account:{0}'.format(file))
+                        sys.stdout.flush()
+                break  # stop the while loop
+
+            counter += 1
+    for num in range(start_num, end_num2 + 1):
+        shortcode = num
+        url = 'http://friendfeed.com/{0}/subscriptions?start={1}'.format(item_value, str((int(shortcode)-1)*72))
+        counter = 0
+
+        while True:
+            # Try 20 times before giving up
+            if counter > 2:
+                # This will stop the script with an error
+                raise Exception('Giving up!')
+
+            try:
+                text = fetch(url)
+            except FetchError:
+                # The server may be overloaded so wait a bit
+                print('Sleeping...')
+                sys.stdout.flush()
+                time.sleep(10)
+            else:
+                if text:
+                    if item_type == 'page':
+                        yield 'page:{0}:{1}'.format(item_value, shortcode)
+                        print('page:{0}:{1}'.format(item_value, shortcode))
+                    elif item_type == 'account':
+                        yield 'account:{0}:{1}'.format(item_value, shortcode)
+                        print('account:{0}:{1}'.format(item_value, shortcode))
+                    sys.stdout.flush()
+
+                    for file in extract_files(text):
+			yield 'account:{0}'.format(file)
+                        print('account:{0}'.format(file))
                         sys.stdout.flush()
                 break  # stop the while loop
 
